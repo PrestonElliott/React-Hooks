957a2ccd845f6079f13e3eca678fd6d20d68e6aa
Preston Elliott <preston.elliott24@gmail.com>
2019-10-30 17:36:43 -0400

    React hooks with a form

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignUpForm from './Components/form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Fragment>
+			<div> 
+				<SignUpForm/> 
+			</div>
+			<div className="App">
+				<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+				Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+				>
+				Learn React
+				</a>
+				</header>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
