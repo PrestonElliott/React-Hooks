f7842b207cab7f92c51e9dd89d7d9d7ef43327bb
Preston Elliott <preston.elliott24@gmail.com>
2019-12-02 12:59:26 -0500

    new form

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -5,6 +5,7 @@ import './StyleSheets/App.css';
 // import HookForm from './Components/Forms/hookForm';
 // import RefactoredHookForm from './Components/Forms/refactoredHookForm'
 // import AbstractHookForm from './Components/Forms/abstractHookForm'
+import CalendarForm from './Components/Forms/calendarForm'
 
 // import ClassCounter from './Components/Counters/classCounter'
 // import HookCounter from './Components/Counters/hookCounter'
@@ -12,7 +13,7 @@ import './StyleSheets/App.css';
 
 // import ToDoHook from './Components/ToDos/toDoHooks'
 
-import FunctionDailyPic from './Components/DataFetch/functionFetchData'
+// import FunctionDailyPic from './Components/DataFetch/functionFetchData'
 // import ClassDailyPic from './Components/DataFetch/classFetchData'
 
 // import DetailsModal from './Components/DataFetch/modal'
@@ -24,8 +25,9 @@ function App() {
 	return (
 		<Fragment>
 			<div> 
+				<CalendarForm/>
 				{/* <DetailsModal /> */}
-				<FunctionDailyPic />
+				{/* <FunctionDailyPic /> */}
 				{/* <ClassDailyPic /> */}
 				{/* <ToDoHook/> */}
 				{/* <HookReducerCounter/> */}
---
+++ react-hooks/src/Components/Forms/calendarForm.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react'
+import { Form, Button, Jumbotron } from 'react-bootstrap'
+
+export default function CalendarForm() {
+
+    // CREATE OBJECTS TO PASS AS ARGUMENTS INTO FUNCTION useFormUpdate 
+    const firstName = useFormUpdate()
+    const lastName = useFormUpdate()
+    const email = useFormUpdate()
+
+    // REFACTORED REACT HOOKS - FORM FIELDS EVENT HANDLER
+    // CUSTOM REACT HOOK useFormUpdate - WORKS FOR ALL FORM FIELDS 
+    function useFormUpdate(initialValue) {
+        const [value, formValue] = useState(initialValue)
+
+        function handleChange(e) {
+            formValue(e.target.value)
+        }
+
+        return {
+            value,
+            onChange: handleChange
+        }
+    }
+
+    // REFACTORED REACT HOOKS - FORM SUBMIT EVENT HANDLER
+    const handleSubmit = (e) => {
+        e.preventDefault()
+ 
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: { 
+                    Accept: 'application/json', 
+                    'Content-Type':'application/json' },
+            body: JSON.stringify({
+                user: {
+                    firstName: firstName.value,
+                    lastName: lastName.value,
+                    email: email.value
+                }
+            })
+        })
+    }
+
+    useEffect(() => { document.title = firstName.value + ' ' + lastName.value })
+
+    return (
+        <>
+        <Jumbotron id="calendar-form">
+            <Form 
+                className="sign-up-form"
+                onSubmit={handleSubmit}
+            >
+
+                <Form.Label className="form-header">
+                    Sign Up Form 
+                </Form.Label>
+
+                <Form.Control 
+                    className="form-fields"
+                    size="lg" 
+                    type="text" 
+                    placeholder="First Name"
+                    {...firstName}
+                />
+
+                <Form.Control 
+                    className="form-fields"
+                    size="lg" 
+                    type="text" 
+                    placeholder="Last Name"
+                    {...lastName}
+                />    
+
+                <Form.Control 
+                    className="form-fields"
+                    size="lg" 
+                    type="text" 
+                    placeholder="Email"
+                    {...email}
+                />
+
+                <Button
+                    className="submit-button" 
+                    variant="primary" 
+                    type="submit"
+                >
+                    Submit
+                </Button>
+            </Form>
+        </Jumbotron>
+        </>      
+    )
+}
